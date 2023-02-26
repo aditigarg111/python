68aabb3ffe74dd5965411acdda5a919a17d4c8fd
aditigarg111 <aditigarg571@gmail.com>
2023-02-26 22:54:51 +0530

    26022023

---
+++ chapter7/dictonaries.py
@@ -0,0 +1,29 @@
+# dictonaries are collection of unordered data in key: value pair
+# 1 way to create dictonary
+# user = {"name":"aditi","age":"22"}
+# print(user)
+# print(type(user))
+
+# 2 way to create dictonary
+# user = dict(name = "aditi", age =22)
+# print(user)
+
+# how to access data in the dictonary
+# there is no indexing as of the unordered collection of data 
+# user = {"name":"aditi","age":"22"}
+# print(user['name'])
+# print(user['age'])
+
+# which type of data can dictonary store
+# user_info={
+#     "name":"aditi",
+#     "age":24,
+#     "fav_movie":["krish","dhoom"]
+# }
+# print(user_info)
+# print(user_info["age"])
+
+# how to add data inside empty dictonary
+user_info2={}
+user_info2["name"]= "aditi"
+print(user_info2)
