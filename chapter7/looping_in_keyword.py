9e0763c45c6d0d149b73dfd515a8ba4a4e25b0af
aditigarg111 <aditigarg571@gmail.com>
2023-02-27 23:59:08 +0530

    27022023

---
+++ chapter7/looping_in_keyword.py
@@ -0,0 +1,30 @@
+user_info={
+    "name":"aditi",
+    "age":24,
+    "fav_movie":["krish","dhoom"]
+}
+# check if key exist in the dictionary
+# if 'name' in user_info:
+#     print("present")
+# else:
+#     print("not present")
+
+# check if value exist in the dictionary
+# if 'aditi' in user_info.values():
+#     print("present")
+# else:
+#     print("not present")
+
+# loops in dictonary
+# for i in user_info: ---- gives the keys
+#     print(i)
+# for i in user_info.values(): #---- gives the values
+#     print(i)
+
+# value method
+# user_info_values = user_info.values()
+# print (user_info_values)
+
+# keys method
+user_info_keys = user_info.keys()
+print (user_info_keys)
