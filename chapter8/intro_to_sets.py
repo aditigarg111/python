ac923699bed5b2c86c84c01a9dbc5879c2132414
aditigarg111 <aditigarg571@gmail.com>
2023-03-03 23:28:26 +0530

    03032023

---
+++ chapter8/intro_to_sets.py
@@ -0,0 +1,29 @@
+# sets - unordred collection of unique items 
+s = {1,2,3,4,2} # only unique items will be printed 
+# print(s)
+# to remove duplicates
+# l = [1,2,3,4,5,5,5,6,3,4,3,8,9]
+# s2 = set(l)
+# print(s2)
+# # convert back into list 
+# s2 = list(set(l))
+# print(s2)
+
+# to add element in set 
+# s.add(6)
+# print(s)
+
+# to remove something from set 
+# s.remove(4) ----- it should have that number inside set otheriwse will throw error 
+# print(s)
+# s.discard(9) # this will not give error evenm though the number is not in the set 
+# print(s)
+
+# clear 
+# s.clear()
+# print(s)
+
+# copy method
+s1 = s.copy()
+print(s)
+print(s1)
---
+++ chapter8/more_about_set.py
@@ -0,0 +1,20 @@
+s = {"a","b","c"}
+#  in keyword in set 
+# if "a" in s:
+#     print ("present")
+# else:
+#     print("not present")    
+
+# for loop 
+# for i in s:
+#     print(i)
+
+# set maths 
+s1 = {1,2,3,4}
+s2 = {2,5,3,7,9}
+# union 
+# union_set = s1|s2
+# print(union_set)
+
+# intersection
+print(s1&s2)
